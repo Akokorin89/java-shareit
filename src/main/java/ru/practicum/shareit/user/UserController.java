f9d38494f5ce61f3c4264036dfb28576f7496475
Alexandr Kokorin <Akokorin89@yandex.ru>
2022-09-18 19:01:09 +0300

    Первая версия ShareIt

--- src/main/java/ru/practicum/shareit/user/UserController.java
+++ src/main/java/ru/practicum/shareit/user/UserController.java
@@ -1,7 +1,14 @@
 package ru.practicum.shareit.user;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.shareit.user.dto.UserDto;
+import ru.practicum.shareit.user.model.User;
+import ru.practicum.shareit.user.model.UserMapper;
+import ru.practicum.shareit.user.service.UserService;
+
+import javax.validation.Valid;
+import java.util.Collection;
+import java.util.stream.Collectors;
 
 /**
  * TODO Sprint add-controllers.
@@ -9,4 +16,40 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(path = "/users")
 public class UserController {
+
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public Collection<UserDto> getAll() {
+        return userService.getAll().stream()
+                .map(UserMapper::toUserDto)
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("/{id}")
+    public UserDto getById(@PathVariable int id) {
+        return UserMapper.toUserDto(userService.getById(id));
+    }
+
+    @PostMapping
+    public UserDto create(@Valid @RequestBody UserDto userDto) {
+        User user = UserMapper.toUser(userDto);
+        return UserMapper.toUserDto(
+                userService.create(user)
+        );
+    }
+
+    @PatchMapping("/{id}")
+    public UserDto update(@PathVariable int id, @RequestBody User user) {
+        return UserMapper.toUserDto(userService.update(id, user));
+    }
+
+    @DeleteMapping("/{id}")
+    public void remove(@PathVariable int id) {
+        userService.remove(id);
+    }
 }
